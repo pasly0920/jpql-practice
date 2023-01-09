9cef2aecbf439e9fab9ae9495c6c491f0d908f2a
pasly0920 <inwoo920621@gmail.com>
2023-01-09 11:58:40 +0900

    feat: 기본 모델 설정

---
+++ src/main/java/jpql/Order.java
@@ -0,0 +1,33 @@
+package jpql;
+
+import javax.persistence.Column;
+import javax.persistence.Embedded;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "ORDERS")
+public class Order {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "ORDER_ID")
+    private Long id;
+
+    private int orderAmount;
+
+    @Embedded
+    private Address address;
+
+    @ManyToOne
+    @JoinColumn(name = "PRODUCT_ID")
+    private Product product;
+}
