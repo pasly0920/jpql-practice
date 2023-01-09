7a1e00116682beea106c2912973e536a3c15e6dd
pasly0920 <inwoo920621@gmail.com>
2023-01-09 11:09:14 +0900

    chore: initial setup

---
+++ src/main/java/jpql/Member.java
@@ -0,0 +1,20 @@
+package jpql;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class Member {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+    private String username;
+    private int age;
+
+}
