4718232b5895300ea4d28513607f24672a659423
pasly0920 <inwoo920621@gmail.com>
2023-01-09 14:59:27 +0900

    feat: 지연 로딩 추가

--- src/main/java/jpql/Team.java
+++ src/main/java/jpql/Team.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
@@ -22,6 +23,6 @@ public class Team {
 
     private String name;
 
-    @OneToMany(mappedBy = "team")
+    @OneToMany(mappedBy = "team", fetch = FetchType.LAZY)
     private List<Member> members = new ArrayList<>();
 }
