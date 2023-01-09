c45e2630d165fc9549df172cddfaec247ef22e6c
pasly0920 <inwoo920621@gmail.com>
2023-01-09 11:43:59 +0900

    clear git cache

--- src/main/java/jpql/Member.java
+++ src/main/java/jpql/Member.java
@@ -1,8 +1,11 @@
 package jpql;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -13,8 +16,13 @@ public class Member {
 
     @Id
     @GeneratedValue
+    @Column(name = "MEMBER_ID")
     private Long id;
     private String username;
     private int age;
+
+    @ManyToOne
+    @JoinColumn(name = "TEAM_ID")
+    private Team team;
 
 }
